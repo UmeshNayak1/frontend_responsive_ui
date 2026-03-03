f8270add8bc6181e559acad0a363052f8e03a999
reactjs_course_projects <umeshlnayak@gmail.com>
2026-03-03 10:29:30 +0530

    updated the server.js file

--- Purchase Form Management System/server.js
+++ Purchase Form Management System/server.js
@@ -168,6 +168,10 @@ app.get("/download", (req, res) => {
 });
 
 /* ------------------ Start Server ------------------ */
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+// app.listen(3000, () => {
+//     console.log("Server running on http://localhost:3000");
+// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
